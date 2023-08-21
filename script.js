77e06ca3bf3179353b43a080b4b8978b77b3efee
vinu <vinu11krish@gmail.com>
2023-08-21 14:58:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("vinu")
+// document.querySelector('.cross').style.display= 'none'
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo')
+})
+document.querySelector('.cross').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo')
+})
